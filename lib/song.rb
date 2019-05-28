e56bf7c6aa390475a1271fc49f79fbfb675abc95
Margot Konig <margot.konig@blackrock.com>
2019-05-28 19:31:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,17 @@
 class Song 
   attr_accessor :name, :artist 
   
+  @@all = []
+  
    def initialize(name)
     @name = name
+    @@all << name
   end
+  
+  def self.all
+    @@all
+    
+    def artist_name
+      self.artist.name
+    end
 end
