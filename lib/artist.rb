b3cb24375aadc8cc13df06c22e04ac771af5bc12
Margot Konig <margot.konig@blackrock.com>
2019-05-28 19:29:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,12 +6,12 @@ class Artist
    def initialize(name)
     @name = name
     @songs = []
-    @@artists << name
   end
   
    def add_song(song)
     @songs << song
     song.artist = self
+     @@song_count +=1
   end
   
    def songs
